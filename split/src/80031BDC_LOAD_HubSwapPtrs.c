ebdef2c8de99a42d5dbb4727b8a2e86cb196a294
DataPlusProgram <dataplusprogram@gmail.com>
2026-05-16 12:46:59 +0100

    Added 2 missing matches.

--- split/src/80031BDC_LOAD_HubSwapPtrs.c
+++ split/src/80031BDC_LOAD_HubSwapPtrs.c
@@ -1,3 +1,22 @@
-#include "../../common.h"
+typedef signed char s8;
+typedef short s16;
+typedef int s32;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/80031BDC_LOAD_HubSwapPtrs/80031BDC_LOAD_HubSwapPtrs.s")
+#define M2C_FIELD(expr, typePtr, offset) (*(typePtr)((s8 *)(expr) + (offset)))
+
+void LOAD_HubSwapPtrs(void *arg0) {
+    s32 temp_a0;
+    s32 temp_a1;
+    s32 temp_v0;
+
+    temp_v0 = M2C_FIELD(arg0, s32 *, 0x164);
+    if (temp_v0 != 0) {
+        temp_a0 = M2C_FIELD(arg0, s32 *, 0x160);
+        temp_a1 = M2C_FIELD(arg0, s32 *, 0x1A38);
+        M2C_FIELD(arg0, s32 *, 0x160) = temp_v0;
+        M2C_FIELD(arg0, s16 *, 0x2548) = 1;
+        M2C_FIELD(arg0, s32 *, 0x164) = temp_a0;
+        M2C_FIELD(arg0, s32 *, 0x1A38) = (s32) M2C_FIELD(arg0, s32 *, 0x1A3C);
+        M2C_FIELD(arg0, s32 *, 0x1A3C) = temp_a1;
+    }
+}
--- split/src/80032438_LOAD_FindFile.c
+++ split/src/80032438_LOAD_FindFile.c
@@ -1,3 +1,16 @@
-#include "../../common.h"
+typedef int s32;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/80032438_LOAD_FindFile/80032438_LOAD_FindFile.s")
+typedef s32 M2C_UNK;
+
+M2C_UNK CDSYS_SetMode_StreamData();                 /* extern */
+s32 CdSearchFile();                         /* extern */
+M2C_UNK LOAD_StringToUpper();                    /* extern */
+
+s32 LOAD_FindFile(s32 arg0, s32 arg1) {
+    if ((arg0 == 0) || (arg1 == 0)) {
+        return 0;
+    }
+    CDSYS_SetMode_StreamData();
+    LOAD_StringToUpper(arg0);
+    return CdSearchFile(arg1, arg0) != 0;
+}
